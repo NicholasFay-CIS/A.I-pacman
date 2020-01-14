ecfd31b4028ef0eac51851d11ecb72730cac925d
Nicholas Fay <nfay@uoregon.edu>
2020-01-14 11:38:36 -0800

    fixing issues with the autograder

--- proj1-search-python3/search.py
+++ proj1-search-python3/search.py
@@ -90,77 +90,61 @@ def depthFirstSearch(problem):
     """
     "*** YOUR CODE HERE ***"
     #util.raiseNotDefined()
-
-    print("Start:", problem.getStartState())
-    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
-    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
-    # initialize the stack  
+    #indeces for start state, path_list (directions) and length
+    start_state_index = 0
+    path_list_index = 1
+    length_index = 2
+    #initialize the stack
     stack = util.Stack()
-    #have a list for visted_branches
-    visited_branches = []
-    #empty list if nothing able to be returned
-    empty_list = []
-    #get the problem start state
-    problem_start_State = problem.getStartState()
-    #path list
-    path_list = []
-    #length
-    length = 0 
-    #variable to symbolize start state xy position index in info lists
-    xy_position = 0
-    #variable to symbolize the path list index in info lists
-    path_ = 1
-    #variable to symbolize length index in info lists
-    length_ = 2
-    #create the root info tuple which includes the start state, path list (of directions) and length
+    #create a named tuple for the root
     root_info = namedtuple('root', 'startState path_list length')
-    root_info.startState = problem_start_State
-    root_info.path_list = path_list
-    root_info.length = length
-    #push the info tuple onto the stack, INITIAL PUSH
-    stack.push(root_info) 
-   
-   #Forever loop
+    problem_start_state = problem.getStartState() # set the start state
+    root_info.startState = problem_start_state 
+    root_info.path_list = [] #initialize an empty list for a path of directions
+    root_info.length = 0 #initialize the length to be zero
+    stack.push(root_info)   #push the info onto the stack (as a single node)
+    expanded_nodes = [] #empty list for visted or expanded sections
+    empty_list = [] 
+
     while(True):
-        #if the stack is empty break out of the loop
+        #if the stack is empty break and return the empty list
         if(stack.isEmpty() == True):
             break
-        #retrieve a child node
-        child_node = stack.pop()
-        #get child node position
-        child_node_xy = child_node.startState
-        #get child node direction
-        direction = child_node.path_list
-        #check is goal state of child node position 
-        is_goal_state_success = problem.isGoalState(child_node_xy)
-
-        #if the goal state is a success return the direction
-        if(is_goal_state_success):
-            return direction
-        #if the goal state is not a success 
-        elif(child_node_xy not in visited_branches):
-            #append the position to visited branches
-            visited_branches.append(child_node_xy)
-            #get the successors of the child nodes position
-            successors = problem.getSuccessors(child_node_xy)
-            #iterate through each successor node
-            for successor_node in successors:
-                #get the nodes start state
-                new_node_startState = successor_node[xy_position]
-                #if the start state is not been visited
-                if(new_node_startState not in visited_branches):
-                    #get the nodes length
-                    new_node_length = successor_node[length_]
-                    #get the nodes directions
-                    new_node_path = direction + [successor_node[path_]]
-                    #create the successor info tuple
-                    successor_info = namedtuple('successor', 'startState path_list length')
-                    successor_info.startState = new_node_startState
-                    successor_info.path_list = new_node_path
-                    successor_info.length = new_node_length
-                    #push the successor info list to the stack
-                    stack.push(successor_info)
-    #if the stack was empty return an empty list
+        else:
+            #if the stack is not empty pop a child node
+            child_node = stack.pop()
+            #is the goal reached
+            reached_goal = problem.isGoalState(child_node.startState)
+            #if the goal is not reached
+            if(not reached_goal):
+                #check if the child node start state has been visited, if so continue
+                if(child_node.startState in expanded_nodes):
+                    continue
+                #if it has not been expanded
+                else:
+                    #get the successors of the nodes start state
+                    successors = problem.getSuccessors(child_node.startState)
+                    #iterate through the successors
+                    for successor in successors:
+                        successor_start_state = successor[start_state_index] #get the start state index of the successor
+                        successor_direction_list = [successor[path_list_index]] #get the successors new path list. example) ['West']
+                        successor_length = successor[length_index] #get the successors length
+                        #check if the successors start state has already been recorded
+                        if(successor_start_state in expanded_nodes):
+                            continue
+                        #otherwise it has not been recorded
+                        else:
+                            #create a next successor named tuple with the same attributes as the named tuple for the root
+                            next_successor = namedtuple('successor', 'startState path_list length')
+                            next_successor.startState = successor_start_state #set the start state
+                            next_successor.path_list = child_node.path_list + successor_direction_list #add the child nodes direction list to the successor nodes direction list. Ex
+                            next_successor.length = successor_length #set the length to be the successors length
+                            stack.push(next_successor) #push the successor node onto the stack
+                    expanded_nodes.append(child_node.startState) #add the child node start state to the visited nodes (nodes we expanded)
+            else:
+                #if the goal has been reached return the direction list of the poped node
+                return child_node.path_list 
+    #return empty list if the stack is empty
     return empty_list
 
 def breadthFirstSearch(problem):
